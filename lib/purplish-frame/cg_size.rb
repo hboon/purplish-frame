0f08d531e90284c7a252e3b4c8edd7624696acbd
Hwee-Boon Yar <hboon@motionobj.com>
2015-12-06 18:35:29 +0800

    Add #square?

--- lib/purplish-frame/cg_size.rb
+++ lib/purplish-frame/cg_size.rb
@@ -4,4 +4,8 @@ class CGSize
   def self.to_native_size(anObject)
     anObject.to_size
   end
+
+  def square?
+    width == height && width > 0
+  end
 end
--- lib/purplish-frame/osx/ns_size.rb
+++ lib/purplish-frame/osx/ns_size.rb
@@ -4,4 +4,8 @@ class NSSize
   def self.to_native_size(anObject)
     anObject.to_size
   end
+
+  def square?
+    width == height && width > 0
+  end
 end
